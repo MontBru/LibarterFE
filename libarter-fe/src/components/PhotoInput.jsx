dde6502fb71bca6dfedc754f2ec88fd46938a5b0
MontBr <monticelli.bryan@gmail.com>
2023-09-25 12:46:23 +0300

    added photo input and deletion of photos

--- libarter-fe/src/components/PhotoInput.jsx
+++ libarter-fe/src/components/PhotoInput.jsx
@@ -1,81 +1,93 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import React, { useState } from 'react';
 
 const PhotoInput = () => {
   const [photos, setPhotos] = useState([]);
-  const [fileInputs, setFileInputs] = useState([0]);
 
-  const handleFileInputChange = (index, event) => {
-    console.log("in handle file input change")
+  const handleFileInputChange = ( event) => {
     const selectedFile = event.target.files[0];
     if (selectedFile) {
       const updatedPhotos = [...photos];
       const reader = new FileReader();
-  
+
       reader.onload = (e) => {
-        console.log(e.target.result); 
-        updatedPhotos[index] = e.target.result;
+        updatedPhotos.push(e.target.result);
         setPhotos(updatedPhotos);
-        console.log(photos)
-        if (index < 4) {
-          // Add a new file input field if there are less than 5 photos
-          setFileInputs([...fileInputs, index + 1]);
-        }
       };
-  
+
       reader.readAsDataURL(selectedFile);
     }
   };
 
+  const handleImageClick = (index) => {
+    // Implement zoom-in functionality here (you can use a modal or lightbox component)
+    // Example: set a state to open a modal with the enlarged image
+  };
+
+  const handleRemoveImage = (index) => {
+    const updatedPhotos = photos.filter((value,ind)=>ind!==index);
+    setPhotos(updatedPhotos);
+  };
+
   const renderPhotoInputs = () => {
-    return fileInputs.map((ind) => (
-      <div key={ind} className="p-2 flex justify-center items-center">
-          {photos[ind] ? (
-            <img 
-            src={photos[ind]} 
-            alt={`Uploaded ${ind}`} 
-            className='w-36 h-36 shadow-mf rounded-md'
+    return photos.map((value,ind) => (
+      <div key={ind} className="p-2">
+          <div className="relative">
+            <img
+              src={photos[ind]}
+              alt={`Uploaded ${ind}`}
+              className="w-64 h-64 shadow-md rounded-md cursor-pointer"
+              onClick={() => handleImageClick(ind)}
             />
-          ) : (
-            <div
-              className="bg-customColors-darkBrown text-white font-bold h-36 w-36 shadow-md rounded-md flex flex-col justify-center items-center text-3xl relative"
+            <button
+              type="button"
+              className="absolute top-0 right-0 p-2 text-red-500 text-2xl rounded-md shadow-md opacity-90"
+              onClick={() => handleRemoveImage(ind)}
             >
-              <label htmlFor={`photo-input-${ind}`} className="cursor-pointer">
-                +
-                <input
-                  type="file"
-                  id={`photo-input-${ind}`}
-                  accept="image/*"
-                  onChange={(e) => {
-                    console.log("changed");
-                    handleFileInputChange(ind, e);
-                  }}
-                  className="opacity-0 absolute inset-0 w-36 h-36"
-                />
-              </label>
-            </div>
-          )}
-          
+              <FontAwesomeIcon icon={faTrash}/>
+            </button>
+          </div>
       </div>
     ));
-    // return (
-    //   <div>
-    //     <input
-    //       type="file"
-    //       id={`photo-input`}
-    //       accept="image/*"
-    //       onChange={(e) => {
-    //           console.log("changed")
-    //       }}
-    //     />
-    //   </div>
-    // );
   };
 
   return (
-    <div>
-      {renderPhotoInputs()}
-    </div>
-  );
+  <div className='grid grid-cols-1
+         custom-1:grid-cols-2 
+         custom-2:grid-cols-3 
+         custom-3:grid-cols-4 
+         custom-4:grid-cols-5 py-4'>
+
+    {renderPhotoInputs()}
+    
+    
+    {photos.length < 5?
+      (
+        <div
+          className="bg-customColors-darkBrown text-white 
+          font-bold h-64 w-64 shadow-md 
+          rounded-md flex justify-center 
+          items-center text-3xl relative
+          m-2"
+        >
+          <label htmlFor={`photo-input`} className='cursor-pointer'>
+            +
+            <input
+              type="file"
+              id={`photo-input`}
+              accept="image/*"
+              onChange={(e) => {
+                handleFileInputChange(e);
+              }}
+              className="opacity-0 absolute inset-0 w-64 h-64"
+            />
+          </label>
+        </div>
+      ):null
+    }
+  </div>
+    );
 };
 
 export default PhotoInput;
