6ddeb8713a7ce7b4260f0811cb189d7a32ab367e
MontBru <80487370+MontBru@users.noreply.github.com>
2024-04-20 16:46:34 +0300

    Update constants.jsx

--- libarter-fe/src/constants.jsx
+++ libarter-fe/src/constants.jsx
@@ -17,10 +17,10 @@ export const routes = {
     messages: '/messages'
 };
 
-// export const dbAdress="https://libarterbe.azurewebsites.net/";
+export const dbAdress="https://libarterbe.azurewebsites.net/";
 
-// export const feAdress = "https://libarter-fe.vercel.app";
+export const feAdress = "https://libarter-fe.vercel.app";
 
-export const dbAdress="http://localhost:8080";
+// export const dbAdress="http://localhost:8080";
 
-export const feAdress = "http://localhost:3000";
+// export const feAdress = "http://localhost:3000";
